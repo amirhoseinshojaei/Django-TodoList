ac7fbda8ae12eb71f228ba82db949914e0cc8468
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-06-02 23:36:47 +0330

    configure CustomUser

--- core/settings.py
+++ core/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#CustomUser
+AUTH_USER_MODEL = 'auths.CustomUser'
