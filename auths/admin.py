3ae851f6171015f67d9e54c2197aad9c8f046595
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-06-03 03:20:30 +0330

    register CustomUser model

--- auths/admin.py
+++ auths/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    list_display = [
+
+        'username','emial'
+        'age','phone'
+    ]
+
+    list_max_show_all = 120
+    list_per_page = 120
+
+    list_display_links = ['username']
