82eaec0be6e83dc75729c6688e35a499b5dc02ac
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-06-03 19:50:38 +0330

    build an index view

--- web/views.py
+++ web/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Todo
+from .forms import TodoForm
 # Create your views here.
+
+
+def index(request):
+
+    item_list = Todo.objects.filter('-published_at')
+
+    if request.method == 'POST':
+
+        form = TodoForm(request.form)
+
+        if form.is_valid:
+
+            form.save()
+            return redirect('todo')
+        
+    else:
+
+        form = TodoForm()
+
+    page = {
+        "forms": form,
+        "list": item_list,
+        "title": "TODO LIST",
+    }
+    return render(request, 'todo/index.html', page)
