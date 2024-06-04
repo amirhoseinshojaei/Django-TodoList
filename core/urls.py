e72d689cf4c05123029615badd89e3e14f999494
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-06-05 01:09:18 +0330

    configure url for signin and signup view

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from web import views
+from auths import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index,name='todo'),
-    path("del/<str:item_id>",views.remove,name='del')
+    path("del/<str:item_id>",views.remove,name='del'),
+    path('sigin/',views.signin,name='signin'),
+    path('signup/',views.signup,name='signup'),
 ]
