b62bedbc312ef6d0f1f471fd1783655dd33935b4
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-06-06 00:17:30 +0330

    build custom password change view

--- auths/views.py
+++ auths/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate,login
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -103,3 +105,29 @@ def logout(request):
 
     logout(request)
     messages.success(request,'Success logout')
+
+
+
+class CustomPasswordResetView(auth_views.PasswordResetView):
+
+    template_name = 'auths/password_reset_form.html'
+    email_template_name = 'auths/password_reset_email.html'
+    success_url = reverse_lazy('password_reset_done')
+
+
+class CustomPasswordResetDoneView(auth_views.PasswordResetDoneView):
+
+    template_name = 'auths/password_reset_done.html'
+
+
+
+class CustomPasswordResetConfirm(auth_views.PasswordResetConfirmView):
+
+    template_name = 'auths/password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_complete')
+
+
+
+class CustomPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+
+    template_name = 'auths/password_reset_complete.html'
